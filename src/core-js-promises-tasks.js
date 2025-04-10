1365689bac5b0b2f078231e0b5df8c5fe247f6d9
Medvedev Kirill <medvedev_k@icloud.com>
2025-04-11 00:12:44 +0500

    feat: implement queuePromises function

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -139,8 +139,14 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuePromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuePromises(promises) {
+  return promises.reduce(
+    (prevPromise, currentPromise) =>
+      prevPromise.then((prevResult) =>
+        currentPromise.then((result) => prevResult + result)
+      ),
+    Promise.resolve('')
+  );
 }
 
 module.exports = {
